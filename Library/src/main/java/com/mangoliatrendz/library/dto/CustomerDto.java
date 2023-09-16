4554a9eaec41c418193722c9b690dd6d4db6dfc2
Sarath R <sarathrajan10@gmail.com>
2023-09-16 11:26:21 +0530

    made changes

--- Library/src/main/java/com/mangoliatrendz/library/dto/CustomerDto.java
+++ Library/src/main/java/com/mangoliatrendz/library/dto/CustomerDto.java
@@ -18,24 +18,21 @@ import java.util.List;
 public class CustomerDto implements Serializable {
 
     private long id;
-    @Size(min = 3, max = 10, message = "First name contains 3-10 characters")
-    @NotEmpty(message = "FirstName should not be empty")
+
     private String firstName;
 
-    @Size(min = 3, max = 10, message = "Last name contains 3-10 characters")
-    @NotEmpty(message = "LastName should not be empty")
+
     private String lastName;
 
-    @NotEmpty(message = "Email should not be empty")
+
     @Email
     private String email;
 
 
-    @Size(min = 10, max = 15, message = "Phone number contains 10-15 characters")
-    @NotEmpty(message = "MobileNumber should not be empty")
+
     private String mobileNumber;
 
-    @Size(min = 3, max = 15, message = "Password contains 3-10 characters")
+    
     private String password;
 
     private boolean is_activated;
